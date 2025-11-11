f5441d6e8d8e27da4238dd226e99032ed7eebcbb
king <jesseqty74@gmail.com>
2025-11-11 12:05:27 +0000

    Implemented email service

--- src/main/java/com/blogwebapp/webapp/controller/EmailController.java
+++ src/main/java/com/blogwebapp/webapp/controller/EmailController.java
@@ -1,6 +1,7 @@
 package com.blogwebapp.webapp.controller;
 
 import com.blogwebapp.webapp.model.EmailDto;
+import com.blogwebapp.webapp.service.EmailService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,10 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/email")
+@RequestMapping("/blog")
 public class EmailController {
+    private EmailService emailService;
+
+    public EmailController(EmailService emailService){
+        this.emailService = emailService;
+    }
+
     @PostMapping("/sendEmail")
     public String sendEmail(@Valid @ModelAttribute("email") EmailDto email){
-        return "redirect:/posts";
+        emailService.sendMail(email);
+        return "redirect:/blog/contact";
     }
 }
--- src/main/java/com/blogwebapp/webapp/service/EmailService.java
+++ src/main/java/com/blogwebapp/webapp/service/EmailService.java
@@ -1,4 +1,32 @@
 package com.blogwebapp.webapp.service;
+import com.blogwebapp.webapp.model.EmailDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
+
+    private JavaMailSender mailSender;
+
+    @Value("spring.mail.username")
+    private String toMail;
+
+    public EmailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
+
+
+
+    public void sendMail(EmailDto emailDto){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setFrom(emailDto.getEmailAddress());
+        simpleMailMessage.setSubject(emailDto.getSubject());
+        simpleMailMessage.setText(emailDto.getMessage());
+        simpleMailMessage.setTo(toMail);
+
+        mailSender.send(simpleMailMessage);
+    }
 }
